ff5abe3fb375b43d17996af6330b435f77601a88
bulatgrzegorz <grzegorz0bulat@gmail.com>
2024-12-09 21:03:51 +0100

    day 9 part 2

--- Day9/Day9.cs
+++ Day9/Day9.cs
@@ -6,7 +6,6 @@ public class Day9
 {
     private const string InputFile = "Day9/Day9.input";
     private const string ExampleInput = "2333133121414131402";
-    private static string[] _digits = "123456789".Select(x => x.ToString()).ToArray();
 
     [Fact]
     public void Example()
@@ -16,6 +15,26 @@ public class Day9
         var result = CompactFirst(memory);
 
         Assert.Equal(1928, result);
+    }
+    
+    [Fact]
+    public void ExampleSecond()
+    {
+        var memory = CreateMemory(ExampleInput).ToArray().AsSpan();
+
+        var result = CompactSecond2(memory);
+
+        Assert.Equal(2858, result);
+    }
+    
+    [Fact]
+    public void Second()
+    {
+        var memory = CreateMemory(File.ReadAllText(InputFile)).ToArray().AsSpan();
+
+        var result = CompactSecond2(memory);
+
+        Assert.Equal(6408966547049, result);
     }
     
     [Fact]
@@ -48,28 +67,97 @@ public class Day9
 
         return result;
     }
-    
-    private static long CompactSecond(Span<string> memory)
+
+    private static long CompactSecond2(Span<string> memory)
     {
+        var mem = memory;
         while (true)
         {
-            var lastDigitIndex = memory.LastIndexOfAnyExcept(".");
-            var firstFreeSpaceIndex = memory.IndexOf(".");
-            
-            if(firstFreeSpaceIndex > lastDigitIndex) break;
+            var lastDigitIndex = mem.LastIndexOfAnyExcept(".");
+            var firstDigitIndex = FirstDigitIndex(mem, lastDigitIndex);
+        
+            var firstFreeSpaceIndex = mem.IndexOf(".");
+            if(firstFreeSpaceIndex < 0) break;
 
-            (memory[firstFreeSpaceIndex], memory[lastDigitIndex]) = (memory[lastDigitIndex], ".");
+            var span = mem[firstFreeSpaceIndex..(lastDigitIndex+1)];
+            while (span.Length > 0 && CompactSecondInternal(span))
+            {
+                firstFreeSpaceIndex = span.IndexOf(".");
+                var lastFreeSpaceIndex = LastIndex(span, firstFreeSpaceIndex);
+                
+                span = span[(lastFreeSpaceIndex+1)..];
+            }
+
+            mem = mem[..firstDigitIndex];
         }
-
+        
         long result = 0;
-        for (var i = 0; i < memory.IndexOf("."); i++)
+        for (var i = 0; i < memory.LastIndexOfAnyExcept(".") + 1; i++)
         {
+            if(memory[i] is ".") continue;
             result += long.Parse(memory[i])*i;
         }
 
         return result;
     }
 
+    private static bool CompactSecondInternal(Span<string> memory)
+    {
+        var lastDigitIndex = memory.LastIndexOfAnyExcept(".");
+        var firstDigitIndex = FirstDigitIndex(memory, lastDigitIndex);
+        
+        var firstFreeSpaceIndex = memory.IndexOf(".");
+        if(firstFreeSpaceIndex < 0) return false;
+        var lastFreeSpaceIndex = LastIndex(memory, firstFreeSpaceIndex);
+        
+        var digitLength = lastDigitIndex - firstDigitIndex + 1;
+        var freeSpaceLength = lastFreeSpaceIndex - firstFreeSpaceIndex + 1;
+        if (lastFreeSpaceIndex > lastDigitIndex)
+        {
+            return false;
+        }
+
+        if (digitLength > freeSpaceLength) return true;
+        
+        var fi = firstFreeSpaceIndex;
+        var dl = lastDigitIndex;
+        for (var i = 0; i < digitLength; i++)
+        {
+            (memory[fi++], memory[dl]) = (memory[dl--], ".");
+        }
+
+        return false;
+
+    }
+
+    private static int FirstDigitIndex(ReadOnlySpan<string> memory, int index)
+    {
+        var digit = memory[index];
+        while (index > 0)
+        {
+            index -= 1;
+            if(memory[index] == digit) continue;
+
+            return index + 1;
+        }
+
+        return index;
+    }
+    
+    private static int LastIndex(ReadOnlySpan<string> memory, int index)
+    {
+        var value = memory[index];
+        while (index < memory.Length - 1)
+        {
+            index += 1;
+            if(memory[index] == value) continue;
+
+            return index - 1;
+        }
+
+        return index;
+    }
+    
     private IEnumerable<string> CreateMemory(string input)
     {
         var fileIndex = 0;
@@ -94,6 +182,4 @@ public class Day9
             }
         }
     }
-
-    private record struct Memory(int Length, int Index, int FileIndex, bool IsFreeSpace);
 }
