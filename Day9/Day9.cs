424fe3b21d2eef5eb1ecae7f69c0ebc709e38383
bulatgrzegorz <grzegorz0bulat@gmail.com>
2024-12-11 18:16:44 +0100

    day 9 refactor

--- Day9/Day9.cs
+++ Day9/Day9.cs
@@ -5,160 +5,77 @@ namespace adventOfCode.Day9;
 public class Day9
 {
     private const string InputFile = "Day9/Day9.input";
-    private const string ExampleInput = "2333133121414131402";
 
-    [Fact]
-    public void Example()
-    {
-        var memory = CreateMemory(ExampleInput).ToArray().AsSpan();
-
-        var result = CompactFirst(memory);
-
-        Assert.Equal(1928, result);
-    }
-    
-    [Fact]
-    public void ExampleSecond()
-    {
-        var memory = CreateMemory(ExampleInput).ToArray().AsSpan();
-
-        var result = CompactSecond2(memory);
-
-        Assert.Equal(2858, result);
-    }
-    
     [Fact]
     public void Second()
     {
-        var memory = CreateMemory(File.ReadAllText(InputFile)).ToArray().AsSpan();
+        var (files, blanks) = CreateMemory2(File.ReadAllText(InputFile));
 
-        var result = CompactSecond2(memory);
+        var lastFile = files.Last();
+        var lastFileIndex = lastFile.Key;
+        while (lastFileIndex > 0)
+        {
+            var file = files[lastFileIndex];
+            foreach (var (i, blank) in blanks.Index())
+            {
+                if (blank.index >= file.index)
+                {
+                    blanks = blanks[..i];
+                    break;
+                }
 
+                if (file.size > blank.size) continue;
+                
+                files[lastFileIndex] = (blank.index, file.size);
+                
+                if (file.size == blank.size) blanks.RemoveAt(i);
+                else blanks[i] = (blank.index + file.size, blank.size - file.size);
+                    
+                break;
+            }
+            
+            lastFileIndex--;
+        }
+
+        var result = files.Sum(x => Enumerable.Range(x.Value.index, x.Value.size).Sum(y => (long)x.Key*y));
+        
         Assert.Equal(6408966547049, result);
     }
     
     [Fact]
     public void First()
     {
-        var memory = CreateMemory(File.ReadAllText(InputFile)).ToArray().AsSpan();
+        var memory = CreateMemory(File.ReadAllText(InputFile)).ToArray();
+        
+        var blanks = memory.Index().Where(x => x.Item is -1).Select(x => x.Index);
 
-        var result = CompactFirst(memory);
+        var memorySpan = memory.AsSpan();
+        foreach (var blank in blanks)
+        {
+            memorySpan = memorySpan[..(memorySpan.LastIndexOfAnyExcept(-1) + 1)];
+            
+            if(blank >= memorySpan.Length) break;
+            memorySpan[blank] = memorySpan[^1];
+            memorySpan = memorySpan[..^1];
+        }
+
+        var result = SumElementAndItemProduct(memorySpan);
 
         Assert.Equal(6384282079460, result);
     }
 
-    private static long CompactFirst(Span<string> memory)
+    private static long SumElementAndItemProduct(Span<int> memorySpan)
     {
-        while (true)
-        {
-            var lastDigitIndex = memory.LastIndexOfAnyExcept(".");
-            var firstFreeSpaceIndex = memory.IndexOf(".");
-            
-            if(firstFreeSpaceIndex > lastDigitIndex) break;
-
-            (memory[firstFreeSpaceIndex], memory[lastDigitIndex]) = (memory[lastDigitIndex], ".");
-        }
-
         long result = 0;
-        for (var i = 0; i < memory.IndexOf("."); i++)
+        for (var i = 0; i < memorySpan.Length; i++)
         {
-            result += long.Parse(memory[i])*i;
+            result += memorySpan[i]*i;
         }
 
         return result;
     }
-
-    private static long CompactSecond2(Span<string> memory)
-    {
-        var mem = memory;
-        while (true)
-        {
-            var lastDigitIndex = mem.LastIndexOfAnyExcept(".");
-            var firstDigitIndex = FirstDigitIndex(mem, lastDigitIndex);
-        
-            var firstFreeSpaceIndex = mem.IndexOf(".");
-            if(firstFreeSpaceIndex < 0) break;
-
-            var span = mem[firstFreeSpaceIndex..(lastDigitIndex+1)];
-            while (span.Length > 0 && CompactSecondInternal(span))
-            {
-                firstFreeSpaceIndex = span.IndexOf(".");
-                var lastFreeSpaceIndex = LastIndex(span, firstFreeSpaceIndex);
-                
-                span = span[(lastFreeSpaceIndex+1)..];
-            }
-
-            mem = mem[..firstDigitIndex];
-        }
-        
-        long result = 0;
-        for (var i = 0; i < memory.LastIndexOfAnyExcept(".") + 1; i++)
-        {
-            if(memory[i] is ".") continue;
-            result += long.Parse(memory[i])*i;
-        }
-
-        return result;
-    }
-
-    private static bool CompactSecondInternal(Span<string> memory)
-    {
-        var lastDigitIndex = memory.LastIndexOfAnyExcept(".");
-        var firstDigitIndex = FirstDigitIndex(memory, lastDigitIndex);
-        
-        var firstFreeSpaceIndex = memory.IndexOf(".");
-        if(firstFreeSpaceIndex < 0) return false;
-        var lastFreeSpaceIndex = LastIndex(memory, firstFreeSpaceIndex);
-        
-        var digitLength = lastDigitIndex - firstDigitIndex + 1;
-        var freeSpaceLength = lastFreeSpaceIndex - firstFreeSpaceIndex + 1;
-        if (lastFreeSpaceIndex > lastDigitIndex)
-        {
-            return false;
-        }
-
-        if (digitLength > freeSpaceLength) return true;
-        
-        var fi = firstFreeSpaceIndex;
-        var dl = lastDigitIndex;
-        for (var i = 0; i < digitLength; i++)
-        {
-            (memory[fi++], memory[dl]) = (memory[dl--], ".");
-        }
-
-        return false;
-
-    }
-
-    private static int FirstDigitIndex(ReadOnlySpan<string> memory, int index)
-    {
-        var digit = memory[index];
-        while (index > 0)
-        {
-            index -= 1;
-            if(memory[index] == digit) continue;
-
-            return index + 1;
-        }
-
-        return index;
-    }
     
-    private static int LastIndex(ReadOnlySpan<string> memory, int index)
-    {
-        var value = memory[index];
-        while (index < memory.Length - 1)
-        {
-            index += 1;
-            if(memory[index] == value) continue;
-
-            return index - 1;
-        }
-
-        return index;
-    }
-    
-    private IEnumerable<string> CreateMemory(string input)
+    private IEnumerable<int> CreateMemory(string input)
     {
         var fileIndex = 0;
         for (var index = 0; index < input.Length; index++)
@@ -168,7 +85,7 @@ public class Day9
             {
                 for (var i = 0; i < length; i++)
                 {
-                    yield return fileIndex.ToString();
+                    yield return fileIndex;
                 }
                 
                 fileIndex++;
@@ -177,9 +94,36 @@ public class Day9
             {
                 for (var i = 0; i < length; i++)
                 {
-                    yield return ".";
+                    yield return -1;
                 }
             }
         }
+    }
+    
+    private (Dictionary<int, (int index, int size)> files, List<(int index, int size)> blanks) CreateMemory2(string input)
+    {
+        var files = new Dictionary<int, (int index, int size)>();
+        var blanks = new List<(int index, int size)>();
+        
+        var fileIndex = 0;
+        var position = 0;
+        for (var index = 0; index < input.Length; index++)
+        {
+            var length = input[index] - '0';
+            if(length is 0) continue;
+            
+            if (index % 2 == 0)
+            {
+                files[fileIndex++] = (position, length);
+            }
+            else
+            {
+                blanks.Add((position, length));
+            }
+            
+            position += length;
+        }
+        
+        return (files, blanks);
     }
 }
