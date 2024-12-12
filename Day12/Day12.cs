896d0705eedbd85fa49c0bb9a7ba53284092e63f
bulatgrzegorz <grzegorz0bulat@gmail.com>
2024-12-12 23:01:47 +0100

    day 12 refactor

--- Day12/Day12.cs
+++ Day12/Day12.cs
@@ -19,6 +19,10 @@ public class Day12
                                         """;
     
     private enum Direction { Up, Down, Left, Right };
+
+    private static Direction[] _allDirections = [Direction.Up, Direction.Down, Direction.Left, Direction.Right];
+    private static Direction[] _upDownDirections = [Direction.Up, Direction.Down];
+    private static Direction[] _leftRightDirections = [Direction.Left, Direction.Right];
     private readonly record struct Coordinate(int Row, int Col)
     {
         public Coordinate Move(Direction direction) => direction switch
@@ -43,7 +47,7 @@ public class Day12
             var currentType = position.Type;
             var coordinate = position.Coordinate;
             var movedCoordinate = coordinate.Move(direction);
-            if (movedCoordinate.Row < 0 || movedCoordinate.Row > _rows - 1 || movedCoordinate.Col < 0 || movedCoordinate.Col > _cols - 1)
+            if (IsOutOfBounds(movedCoordinate))
             {
                 moved = default;
                 return false;
@@ -58,10 +62,9 @@ public class Day12
         public int CountNeighbors(char type, HashSet<Coordinate> field)
         {
             var result = 0;
-            Direction[] directions = [Direction.Up, Direction.Down, Direction.Left, Direction.Right];
             foreach (var coordinate in field)
             {
-                foreach (var direction in directions)
+                foreach (var direction in _allDirections)
                 {
                     if(HasNeighbors(type, coordinate, direction)) result++;
                 }
@@ -83,26 +86,18 @@ public class Day12
     public void First()
     {
         var map = GetMap(File.ReadAllLines(InputFile));
-        // var map = GetMap(InputExample.Split(Environment.NewLine));
         
         var visited = new HashSet<Coordinate>();
-        var typeVisited = new List<(char, HashSet<Coordinate>)>();
+        var typeVisited = new List<(char type, HashSet<Coordinate> traversed)>();
         while (FindNextNotVisited(map, visited, out var next))
         {
             var traversed = Traverse(map, next);
             typeVisited.Add((next.Type, traversed));
             
-            visited = [..visited, ..traversed];
+            visited.UnionWith(traversed);
         }
 
-        var result = 0;
-        foreach (var (type, coordinates) in typeVisited)
-        {
-            var neighbors = map.CountNeighbors(type, coordinates);
-            result += coordinates.Count * neighbors;
-
-            Console.WriteLine($"Type: {type} coordinates: {coordinates.Count} and neirberis: {neighbors}");
-        }
+        var result = typeVisited.Select(x => map.CountNeighbors(x.type, x.traversed) * x.traversed.Count).Sum();
         
         Assert.Equal(1573474, result);
     }
@@ -111,21 +106,18 @@ public class Day12
     public void Second()
     {
         var map = GetMap(File.ReadAllLines(InputFile));
-        // var map = GetMap(InputExample.Split(Environment.NewLine));
         
         var visited = new HashSet<Coordinate>();
-        var typeVisited = new List<(char, HashSet<Coordinate>)>();
+        var typeVisited = new List<(char type, HashSet<Coordinate> traversed)>();
         while (FindNextNotVisited(map, visited, out var next))
         {
             var traversed = Traverse(map, next);
             typeVisited.Add((next.Type, traversed));
             
-            visited = [..visited, ..traversed];
+            visited.UnionWith(traversed);
         }
 
         var result = 0;
-        Direction[] upDown = [Direction.Up, Direction.Down];
-        Direction[] rightLeft = [Direction.Left, Direction.Right];
         foreach (var (type, coordinates) in typeVisited)
         {
             var fences = 0;
@@ -133,28 +125,28 @@ public class Day12
             var rows = coordinates.GroupBy(x => x.Row).OrderBy(x => x.Key);
             foreach (var row in rows)
             {
-                foreach (var direction in upDown)
+                foreach (var direction in _upDownDirections)
                 {
                     var colsWithNeighbors = row.Where(x => map.HasNeighbors(type, x, direction))
                         .Select(x => x.Col)
                         .Order()
                         .ToList();
 
-                    fences += FindContinuousGroups(colsWithNeighbors).Count;    
+                    fences += FindContinuousGroups(colsWithNeighbors);    
                 }
             }
             
             var cols = coordinates.GroupBy(x => x.Col).OrderBy(x => x.Key);
             foreach (var col in cols)
             {
-                foreach (var direction in rightLeft)
+                foreach (var direction in _leftRightDirections)
                 {
                     var rowsWithNeighbors = col.Where(x => map.HasNeighbors(type, x, direction))
                         .Select(x => x.Row)
                         .Order()
                         .ToList();
 
-                    fences += FindContinuousGroups(rowsWithNeighbors).Count;
+                    fences += FindContinuousGroups(rowsWithNeighbors);
                 }
             }
             
@@ -163,52 +155,24 @@ public class Day12
         
         Assert.Equal(966476, result);
     }
-    
-    public static List<List<int>> FindContinuousGroups(List<int> numbers)
-    {
-        if (numbers.Count == 0) return [];
-        
-        var groups = new List<List<int>>();
 
-        var currentGroup = new List<int> { numbers[0] };
-
-        for (int i = 1; i < numbers.Count; i++)
+    private static int FindContinuousGroups(List<int> numbers) =>
+        numbers switch
         {
-            if (numbers[i] == numbers[i - 1] + 1)
-            {
-                // Continue the group
-                currentGroup.Add(numbers[i]);
-            }
-            else
-            {
-                // End the current group and start a new one
-                groups.Add(currentGroup);
-                currentGroup = new List<int> { numbers[i] };
-            }
-        }
-
-        // Add the last group
-        groups.Add(currentGroup);
-
-        return groups;
-    }
-
-    private static void Traverse(Map map, Position position, Direction direction, HashSet<Coordinate> alreadyVisited)
-    {
-        if(!map.Move(position, direction, alreadyVisited, out var newPosition)) return;
-
-        Traverse(map, newPosition, alreadyVisited);
-    }
+            [] => 0,
+            _ => numbers.Zip(numbers.Skip(1), (a, b) => b == a + 1 ? 0 : 1).Sum() + 1
+        };
 
     private static HashSet<Coordinate> Traverse(Map map, Position position, HashSet<Coordinate>? visited = null)
     {
-        visited ??= new HashSet<Coordinate>();
+        visited ??= [];
         visited.Add(position.Coordinate);
 
-        Traverse(map, position, Direction.Up, visited);
-        Traverse(map, position, Direction.Down, visited);
-        Traverse(map, position, Direction.Right, visited);
-        Traverse(map, position, Direction.Left, visited);
+        foreach (var direction in _allDirections)
+        {
+            if(!map.Move(position, direction, visited, out var newPosition)) continue;
+            Traverse(map, newPosition, visited);
+        }
 
         return visited;
     }
@@ -231,10 +195,5 @@ public class Day12
         return false;
     }
     
-    private static Map GetMap(string[] input)
-    {
-        return new Map(input
-            .Select(x => x.ToArray())
-            .ToArray());
-    }
+    private static Map GetMap(string[] input) => new(input.Select(x => x.ToArray()).ToArray());
 }
