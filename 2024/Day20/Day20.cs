8dfbef48feeeafaf9777916f497ba8b67d915bac
bulatgrzegorz <grzegorz0bulat@gmail.com>
2024-12-21 00:08:16 +0100

    day 20 - there is only one path! Based on encse. I was calculating different thing...

--- 2024/Day20/Day20.cs
+++ 2024/Day20/Day20.cs
@@ -1,29 +1,9 @@
-using System.Collections.Frozen;
-using System.Runtime.InteropServices;
 using Xunit;
-using Distance = int;
 
 namespace adventOfCode._2024.Day20;
 
 public class Day20
 {
-    private const string ExampleInput = """
-                                        ###############
-                                        #...#...#.....#
-                                        #.#.#.#.#.###.#
-                                        #S#...#.#.#...#
-                                        #######.#.#.###
-                                        #######.#.#...#
-                                        #######.#.###.#
-                                        ###..E#...#...#
-                                        ###.#######.###
-                                        #...###...#...#
-                                        #.#####.#.###.#
-                                        #.#...#.#.#...#
-                                        #.#.#.#.#.#.###
-                                        #...#...#...###
-                                        ###############
-                                        """;
     public enum Direction { Up, Left, Down, Right }
     private static readonly Direction[] AllDirections = [Direction.Up, Direction.Down, Direction.Left, Direction.Right];
     public readonly record struct Coordinate(int Row, int Col)
@@ -36,61 +16,16 @@ public class Day20
             Direction.Right => this with { Col = Col + 1 },
             _ => throw new ArgumentOutOfRangeException(nameof(direction), direction, null)
         };
-
-        public override string ToString() => $"({Row},{Col})";
-
-        public bool Move(Direction direction, int rows, int cols, out Coordinate moved)
-        {
-            moved = direction switch
-            {
-                Direction.Up => this with { Row = Row - 1 },
-                Direction.Down => this with { Row = Row + 1 },
-                Direction.Left => this with { Col = Col - 1 },
-                Direction.Right => this with { Col = Col + 1 },
-                _ => throw new ArgumentOutOfRangeException(nameof(direction), direction, null)
-            };
-
-            return !moved.IsOutOfBounds(rows, cols);
-        }
-
-        private bool IsOutOfBounds(int rows, int cols) => Row < 0 || Row > rows - 1 || Col < 0 || Col > cols - 1;
     }
-
+    
     [Fact]
-    public void Test()
-    {
-        // var input = ExampleInput.Split(Environment.NewLine).Select(x => x.ToCharArray()).ToArray(); 
+    public void First()
+    { 
         var input = InputHelper.GetInputLines().Select(x => x.ToCharArray()).ToArray();
         
-        Dictionary<Coordinate, char> matrix = [];
-        for (var r = 0; r < input.Length; r++)
-        {
-            for (var c = 0; c < input[0].Length; c++)
-            {
-                matrix.Add(new Coordinate(r, c), input[r][c]);
-            }
-        }
+        var cheats = Calculate(input, 2);
 
-        Coordinate start = matrix.First(x => x.Value == 'S').Key;
-        Coordinate end = matrix.First(x => x.Value == 'E').Key;
-        
-        var cheatsPositions = FindAllCheats(matrix, input.Length, input[0].Length);
-        var walls = matrix.Where(x => x.Value is '#').Select(x => x.Key).ToFrozenSet();
-
-        var result = 0L;
-        var baseDistance = Dijkstra(walls, start, end);
-        foreach (var cheatPosition in cheatsPositions.Distinct())
-        {
-            var wallsWithoutCheat = walls.Except([cheatPosition]).ToFrozenSet();
-            var distanceWithCheat = Dijkstra(wallsWithoutCheat, start, end);
-            
-            if (distanceWithCheat <= baseDistance - 100)
-            {
-                result++;
-            }
-        }
-        
-        Assert.Equal(1321, result);
+        Assert.Equal(1321, cheats);
     }
     
     [Fact]
@@ -98,6 +33,13 @@ public class Day20
     { 
         var input = InputHelper.GetInputLines().Select(x => x.ToCharArray()).ToArray();
         
+        var cheats = Calculate(input, 20);
+
+        Assert.Equal(971737, cheats);
+    }
+
+    private static int Calculate(char[][] input, int maxCheatDistance)
+    {
         Dictionary<Coordinate, char> matrix = [];
         for (var r = 0; r < input.Length; r++)
         {
@@ -106,50 +48,33 @@ public class Day20
                 matrix.Add(new Coordinate(r, c), input[r][c]);
             }
         }
-
+        
         Coordinate start = matrix.First(x => x.Value == 'S').Key;
         Coordinate end = matrix.First(x => x.Value == 'E').Key;
+        var path = GetPath(start, end, matrix);
+        var pathIndexes = Enumerable.Range(0, path.Length).ToArray();
         
-        var walls = matrix.Where(x => x.Value is '#').Select(x => x.Key).ToFrozenSet();
-
-        var baseDistance = Dijkstra(walls, start, end);
-        var threshold = 100;
-        var save = baseDistance - threshold;
-        
-        var notWalls = matrix.Where(x => x.Value is not '#').Select(x => x.Key).ToHashSet();
-        var ll = 0;
-        
-        var cache = new Dictionary<(Coordinate, Coordinate), int>();
-        foreach (var notWall in notWalls)
+        int Cheats(int i)
         {
-            if (!cache.TryGetValue((start, notWall), out var startToCheatBeginning))
+            var result = 0;
+            foreach (var j in pathIndexes[..i])
             {
-                startToCheatBeginning = Dijkstra(walls, start,  notWall);
-                cache[(start, notWall)] = startToCheatBeginning;
+                var cheatDistance = ManhattanDistance(path[i], path[j]);
+                // end---------i<---cheat--->j-------start
+                //   ^----x----^             ^
+                //   ^----------y------------|
+                // saving is difference between normal distance from j to i and cheat distance
+                // to get distance from j to i we have to:
+                // calculate difference between x and y (which is (path.Length - j) - (path.Length - i) = i - j
+                var saving = (i - j) - cheatDistance;
+                if (cheatDistance <= maxCheatDistance && saving >= 100) result++;
             }
             
-            foreach (var other in notWalls.Where(x => x != notWall))
-            {
-                var cheatDist = ManhattanDistance(other, notWall);
-                
-                if(cheatDist is > 20 or < 1) continue;
-                if(startToCheatBeginning + cheatDist > save) continue;
-                
-                if (!cache.TryGetValue((end, other), out var endToCheatEnd))
-                {
-                    var man = ManhattanDistance(end, other);
-                    if(startToCheatBeginning + cheatDist + man > save) continue;
-                    endToCheatEnd = Dijkstra(walls, end,  other);
-                    cache[(end, other)] = endToCheatEnd;
-                }
-                
-                if(startToCheatBeginning + cheatDist + endToCheatEnd > save) continue;
-
-                ll++;
-            }
+            return result;
         }
-        
-        Assert.Equal(971737, ll);
+
+        var cheats = pathIndexes.AsParallel().Select(Cheats).Sum();
+        return cheats;
     }
 
     private static int ManhattanDistance(Coordinate start, Coordinate end)
@@ -157,73 +82,24 @@ public class Day20
         return Math.Abs(start.Row - end.Row) + Math.Abs(start.Col - end.Col);
     }
 
-    private IEnumerable<Coordinate> FindAllCheats(Dictionary<Coordinate, char> matrix, int rows, int cols)
+    private static Coordinate[] GetPath(Coordinate start, Coordinate end, Dictionary<Coordinate, char> matrix)
     {
-        foreach (var (coordinate, value) in matrix)
+        List<Coordinate> path = [start];
+        var (previous, current) = (default(Coordinate), start);
+        while (current != end)
         {
-            if(value is not '#') continue;
-
-            if (coordinate.Move(Direction.Right, rows, cols, out var right) && 
-                matrix[right] is not '#' &&
-                coordinate.Move(Direction.Left, rows, cols, out var left) &&
-                matrix[left] is not '#') 
+            foreach (var direction in AllDirections)
             {
-                yield return coordinate;
-            }
-            else if (coordinate.Move(Direction.Up, rows, cols, out var up) && 
-                matrix[up] is not '#' &&
-                coordinate.Move(Direction.Down, rows, cols, out var down) &&
-                matrix[down] is not '#') 
-            {
-                yield return coordinate;
-            }
-        }
-    }
-    
-    private static int Dijkstra(FrozenSet<Coordinate> walls, Coordinate start, Coordinate end)
-    {
-        var queue = new PriorityQueue<Coordinate, Distance>();
-
-        var distances = new Dictionary<Coordinate, (Coordinate? previous, Distance distance)>
-        {
-            [start] = (null, 0)
-        };
-
-        queue.Enqueue(start, 0);
-        
-        while (queue.Count > 0)
-        {
-            var current = queue.Dequeue();
-            
-            if (current == end) return distances[current].distance;
-            var (_, currentDistance) = CollectionsMarshal.GetValueRefOrAddDefault(distances, current, out _);
-            foreach (var edge in GetNeighbours(walls, current))
-            {
-                ref var edgeNode = ref CollectionsMarshal.GetValueRefOrAddDefault(distances, edge.Coordinate, out var edgeExisted);
-
-                var newDistance = currentDistance + edge.distance;
-
-                if (!edgeExisted || newDistance < edgeNode.distance)
-                {
-                    edgeNode = (current, newDistance);
-                    
-                    queue.Remove(edge.Coordinate, out _, out _);
-                    queue.Enqueue(edge.Coordinate, newDistance);
-                }
-            }
-        }
-
-        return 0;
-    }
-    
-    private static IEnumerable<(Coordinate Coordinate, Distance distance)> GetNeighbours(FrozenSet<Coordinate> walls, Coordinate coordinate)
-    {
-        foreach (var direction in AllDirections)
-        {
-            var movedCoordinate = coordinate.Move(direction);
-            if(walls.Contains(movedCoordinate)) continue;
+                var moved = current.Move(direction);
+                if(matrix[moved] is '#') continue;
+                if(moved == previous) continue;
                 
-            yield return (movedCoordinate, 1);
+                (previous, current) = (current, moved);
+                path.Add(current);
+                break;
+            }
         }
+
+        return path.ToArray();
     }
 }
