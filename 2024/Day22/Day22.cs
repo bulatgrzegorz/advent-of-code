05cdcce875d5fe7e459466f24ba1faf14bb31ee7
bulatgrzegorz <grzegorz0bulat@gmail.com>
2024-12-22 22:16:04 +0100

    day 22 - cleaning and optimalizations

--- 2024/Day22/Day22.cs
+++ 2024/Day22/Day22.cs
@@ -1,4 +1,7 @@
+using System.Collections.Concurrent;
+using System.Collections.Frozen;
 using Xunit;
+using PriceChange = (int D1, int D2, int D3, int D4);
 
 namespace adventOfCode._2024.Day22;
 
@@ -23,22 +26,23 @@ public class Day22
     public void Example()
     {
         var buyerSecrets = InputHelper.GetInputLines().Select(long.Parse).ToArray();
-        var buyersPriceChanges = new List<Dictionary<PriceChange, int>>(buyerSecrets.Length);
-        
-        foreach (var buyerSecret in buyerSecrets)
+        var buyersPriceChanges = new FrozenDictionary<PriceChange, int>[buyerSecrets.Length];
+
+        var prices = new int[2000];
+        for (var b = 0; b < buyerSecrets.Length; b++)
         {
-            var prices = new int[2000];
+            var buyerSecret = buyerSecrets[b];
             prices[0] = LastDigit(buyerSecret);
             var previous = buyerSecret;
             for (var i = 1; i < prices.Length; i++)
             {
                 var secret = CalculateSecret(previous);
-                var price =  LastDigit(secret);
+                var price = LastDigit(secret);
 
                 (previous, prices[i]) = (secret, price);
             }
 
-            buyersPriceChanges.Add(CalculatePriceChanges(prices));
+            buyersPriceChanges[b] = CalculatePriceChanges(prices);
         }
 
         var alreadyTriedPriceChanges = new HashSet<PriceChange>();
@@ -49,14 +53,8 @@ public class Day22
             foreach (var priceChange in buyersPriceChanges[i])
             {
                 if (!alreadyTriedPriceChanges.Add(priceChange.Key)) continue;
-                
-                var result = priceChange.Value;
-                for (var j = 0; j < buyerSecrets.Length; j++)
-                {
-                    if(j == i) continue;
 
-                    if (buyersPriceChanges[j].TryGetValue(priceChange.Key, out var price)) result += price;
-                }
+                var result = buyersPriceChanges.AsParallel().Select(x => x.GetValueOrDefault(priceChange.Key, 0)).Sum();
 
                 if (biggestForPriceChange < result) biggestForPriceChange = result;
             }
@@ -70,7 +68,7 @@ public class Day22
         Assert.Equal(1455, finalResult);
     }
     
-    private static Dictionary<PriceChange, int> CalculatePriceChanges(int[] prices)
+    private static FrozenDictionary<PriceChange, int> CalculatePriceChanges(int[] prices)
     {
         Dictionary<PriceChange, int> result = [];
         for (var i = 4; i < prices.Length; i++)
@@ -81,17 +79,14 @@ public class Day22
             result.TryAdd(new PriceChange(d1, d2, d3, d4), s4);
         }
         
-        return result;
+        return result.ToFrozenDictionary();
     }
-
-    private readonly record struct PriceChange(int D1, int D2, int D3, int D4);
 
     private static int LastDigit(long value) => (int)(value % 10);
     private static long CalculateSecret(long s)
     {
         // 16777216 = 2^24 => 2^24 - 1 = 0xFFFFFF
         // n % 2^m = n & 2^m - 1
-        
         s = (s ^ (s << 6)) & 0xFFFFFF;
         s = (s ^ (s >> 5)) & 0xFFFFFF;
         return (s ^ (s << 11)) & 0xFFFFFF;
